7a205d1323dc51a42f172a1fd5408d7efbd30ccd
Ervin Melara <ervin.melara@wework.com>
2018-02-12 23:57:22 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,13 @@
-
 def display_rainbow(color)
+  
   # Write your #display_rainbow method here
+
+
+display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
+red = "R"
+orange = "O"
+yellow = "Y"
+green = "G"
+blue = "B"
+indigo = "I"
+violet = "V"

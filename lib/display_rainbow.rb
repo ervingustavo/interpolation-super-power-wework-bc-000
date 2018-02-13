d9e10bb1b793b108990d564cfc48b6bcd48af8a4
Ervin Melara <ervin.melara@wework.com>
2018-02-13 00:07:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 def display_rainbow(color)
-  puts "#{color[0]}"
-  
+  puts "#{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}"
+end
+
   # Write your #display_rainbow method here
 
 
